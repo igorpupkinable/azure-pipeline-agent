b66696150d48021524830ab9d5e2395aed3911f4
Igor Pupkin <igorpupkinable@users.noreply.github.com>
2026-03-20 18:48:26 +0200

    install Azure CLI and extensions in one script

--- images/ubuntu/scripts/build/install-azure-cli.sh
+++ images/ubuntu/scripts/build/install-azure-cli.sh
@@ -7,7 +7,13 @@
 # Install Azure CLI (instructions taken from https://docs.microsoft.com/en-us/cli/azure/install-azure-cli)
 curl -fsSL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-echo "azure-cli https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt" >> $HELPER_SCRIPTS/apt-sources.txt
+# Source the helpers for use with the script
+source $HELPER_SCRIPTS/etc-environment.sh
 
-rm -f /etc/apt/sources.list.d/azure-cli.list
-rm -f /etc/apt/sources.list.d/azure-cli.list.save
+# AZURE_EXTENSION_DIR shell variable defines where modules are installed
+# https://docs.microsoft.com/en-us/cli/azure/azure-cli-extensions-overview
+export AZURE_EXTENSION_DIR=/opt/az/extensions
+set_etc_environment_variable "AZURE_EXTENSION_DIR" "${AZURE_EXTENSION_DIR}"
+
+# install azure devops Cli extension
+az extension add --name azure-devops
--- images/ubuntu/templates/build.ubuntu-22_04.pkr.hcl
+++ images/ubuntu/templates/build.ubuntu-22_04.pkr.hcl
@@ -52,7 +52,6 @@ build {
     execute_command  = "sudo sh -c '{{ .Vars }} {{ .Path }}'"
     scripts          = [
       "${path.root}/../scripts/build/install-azure-cli.sh",
-      "${path.root}/../scripts/build/install-azure-devops-cli.sh",
       "${path.root}/../scripts/build/install-git.sh",
       "${path.root}/../scripts/build/configure-dpkg.sh"
     ]
