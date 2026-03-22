b038e185f876765bf60e6f86cd616848a9bee1fe
Igor Pupkin <igorpupkinable@users.noreply.github.com>
2026-03-23 01:01:56 +0200

    Add comment

--- images/ubuntu/scripts/build/install-apt-packages.sh
+++ images/ubuntu/scripts/build/install-apt-packages.sh
@@ -4,6 +4,8 @@
 ##  Desc:  Install required packages using APT
 ################################################################################
 
+# Install apt-utils as well
+# See https://github.com/tianon/docker-brew-ubuntu-core/issues/59
 apt-get update
 apt-get --yes install \
   apt-utils curl apt-transport-https software-properties-common
