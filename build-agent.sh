068f04ce12fabb2a0eb3a700a20261f73663f9fc
Igor Pupkin <igorpupkinable@users.noreply.github.com>
2026-06-17 21:01:09 +0300

    Extract variables

--- build-agent.sh
+++ build-agent.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-set -a
-# Destination image for Packer
-[[ $PKR_VAR_destination_image_version == 0.0.* ]] && GALLERY_STORAGE_CONFIG=(true LRS) || GALLERY_STORAGE_CONFIG=(false ZRS)
-PKR_VAR_destination_image_shallow_replication=${GALLERY_STORAGE_CONFIG[0]}
-PKR_VAR_destination_image_storage="Standard_${GALLERY_STORAGE_CONFIG[1]}"
-
 packer build -only "${BUILD_NAME:-ubuntu-22_04}.azure-arm.image" $TEMPLATES_DIR
-set +a
