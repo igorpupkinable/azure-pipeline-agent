882ec2781e1da59a656d253d07e2c06977ff5c22
Igor <igorpupkinable@users.noreply.github.com>
2026-06-17 23:14:43 +0300

    Use variable instead

--- images/ubuntu/scripts/build/list-dpkg.sh
+++ images/ubuntu/scripts/build/list-dpkg.sh
@@ -3,6 +3,7 @@
 ##  File:  list-dpkg.sh
 ##  Desc:  List all installed dpkg packages
 ################################################################################
+FILENAME=/tmp/installed-packages.txt
 
-echo "Listing all installed dpkg packages:"
-dpkg-query -W -f='${Package} ${Version}\n' | sort > installed-packages.txt
+echo "Dump list of all installed dpkg packages to $FILENAME"
+dpkg-query -W -f='${Package} ${Version}\n' | sort > $FILENAME
